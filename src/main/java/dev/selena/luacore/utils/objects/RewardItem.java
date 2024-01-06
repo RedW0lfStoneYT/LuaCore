33164f6f550207e09a23075ae6419c2a2c999f8d
Selena Rose Hammond <selenarosehammond@gmail.com>
2024-01-06 19:46:14 +1300

    Added RewardItem.java can be used for the RandomCollection as a reward

---
+++ src/main/java/dev/selena/luacore/utils/objects/RewardItem.java
@@ -0,0 +1,48 @@
+package dev.selena.luacore.utils.objects;
+
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nullable;
+
+/**
+ * An example class for the {@link dev.selena.luacore.utils.RandomCollection} class
+ */
+@Getter
+public class RewardItem {
+
+    private final ItemStack rewardItem;
+    private final String rewardName;
+    private final String rewardCommand;
+
+
+    /**
+     * Used to setup the reward
+     * @param rewardItem The item if you want it to be item
+     * @param rewardName The Reward name can be used for messages
+     * @param rewardCommand If you want the reward to have a command
+     */
+    public RewardItem(@Nullable ItemStack rewardItem, @Nullable String rewardName, @Nullable String rewardCommand) {
+        this.rewardItem = rewardItem;
+        this.rewardName = rewardName;
+        this.rewardCommand = rewardCommand;
+    }
+
+    /**
+     * Checks if the reward has an item set
+     * @return True if there is an item set
+     */
+    public boolean isItemReward() {
+        return rewardItem != null;
+    }
+
+    /**
+     * Checks if the reward has a command set
+     * @return True if there is a command set
+     */
+    public boolean isCommandReward() {
+        return rewardCommand != null;
+    }
+
+}
